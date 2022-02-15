47b90e8ed9f6a74a9c37c7a3510b831158b40762
ricardocastanho <ricardinhorl@hotmail.com>
2022-02-15 15:03:48 -0300

    ✨ add footer in home page

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,6 +1,9 @@
 <template>
-  <HomeHeader />
-  <HomeContact />
+  <HomePageHeader
+    @login="handleLogin"
+    @create-account="handleCreateAccount"
+  />
+  <HomePageContact />
   <div class="flex justify-center py-10 bg-brand-gray">
     <p class="font-medium text-center text-gray-800">
       Feedbacker © 2022
@@ -9,16 +12,45 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 
-import HomeContact from './Contact.vue'
-import HomeHeader from './Header.vue'
+import HomePageContact from './Contact.vue'
+import HomePageHeader from './Header.vue'
+
+interface SetupReturn {
+  handleLogin(): void
+  handleCreateAccount(): void
+}
 
 export default defineComponent({
   name: 'HomePage',
   components: {
-    HomeHeader,
-    HomeContact
+    HomePageHeader,
+    HomePageContact
+  },
+  setup (): SetupReturn {
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+
+      if (token) {
+        const router = useRouter()
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    const handleLogin = () => {
+      console.log('handleLogin')
+    }
+
+    const handleCreateAccount = () => {
+      console.log('handleCreateAccount')
+    }
+
+    return {
+      handleLogin,
+      handleCreateAccount
+    }
   }
 })
 </script>
