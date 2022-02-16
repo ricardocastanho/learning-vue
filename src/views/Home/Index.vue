2f4bd56a6c4a6ee0c12e3d8cab92931fedb517a6
ricardocastanho <ricardinhorl@hotmail.com>
2022-02-16 15:53:00 -0300

    🔥 remove code from views/Home/Index.vue

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,8 +1,5 @@
 <template>
-  <HomePageHeader
-    @login="handleLogin"
-    @create-account="handleCreateAccount"
-  />
+  <HomePageHeader />
   <HomePageContact />
   <div class="flex justify-center py-10 bg-brand-gray">
     <p class="font-medium text-center text-gray-800">
@@ -12,45 +9,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue'
-import { useRouter } from 'vue-router'
+import { defineComponent } from 'vue'
 
 import HomePageContact from './Contact.vue'
 import HomePageHeader from './Header.vue'
-
-interface SetupReturn {
-  handleLogin(): void
-  handleCreateAccount(): void
-}
 
 export default defineComponent({
   name: 'HomePage',
   components: {
     HomePageHeader,
     HomePageContact
-  },
-  setup (): SetupReturn {
-    onMounted(() => {
-      const token = window.localStorage.getItem('token')
-
-      if (token) {
-        const router = useRouter()
-        router.push({ name: 'Feedbacks' })
-      }
-    })
-
-    const handleLogin = () => {
-      console.log('handleLogin')
-    }
-
-    const handleCreateAccount = () => {
-      console.log('handleCreateAccount')
-    }
-
-    return {
-      handleLogin,
-      handleCreateAccount
-    }
   }
 })
 </script>
